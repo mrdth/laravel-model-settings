ebeb4a89c88f3221ae07ad9c66e77499fa072f40
mrdth <mrdth@zeventien.org>
2024-07-25 17:55:15 +0100

    Make database column used to store settings configurable

--- src/Concerns/HasSettings.php
+++ src/Concerns/HasSettings.php
@@ -9,34 +9,37 @@ use Illuminate\Support\Facades\Schema;
  */
 trait HasSettings
 {
+    private string $settings_column;
+
     // This method is called when the trait is booted, via some Laravel magic.
     public function initializeHasSettings(): void
     {
+        $this->settings_column = config('model-settings.column', 'settings');
         $this->checkSettingsExist();
 
-        $this->mergeFillable(['settings']);
-        $this->mergeCasts(['settings' => 'array']);
+        $this->mergeFillable([$this->settings_column]);
+        $this->mergeCasts([$this->settings_column => 'array']);
     }
 
     public function hasSetting($name): bool
     {
-        return isset($this->settings[$name]);
+        return isset($this->{$this->settings_column}[$name]);
     }
 
     public function getSettings(): array
     {
-        return $this->settings;
+        return $this->{$this->settings_column};
     }
 
     public function deleteSettings(): void
     {
-        $this->settings = null;
+        $this->{$this->settings_column} = null;
         $this->save();
     }
 
     public function getSetting(string $name)
     {
-        return $this->settings[$name] ?? null;
+        return $this->{$this->settings_column}[$name] ?? null;
     }
 
     public function addSetting(string $name, $value): void
@@ -51,30 +54,30 @@ trait HasSettings
 
     public function updateSetting(string $name, $value): void
     {
-        $settings = $this->settings;
+        $settings = $this->{$this->settings_column};
         $settings[$name] = $value;
-        $this->settings = $settings;
+        $this->{$this->settings_column} = $settings;
         $this->save();
     }
 
     public function deleteSetting(string $name): void
     {
-        $settings = $this->settings;
+        $settings = $this->{$this->settings_column};
         unset($settings[$name]);
-        $this->settings = $settings;
+        $this->{$this->settings_column} = $settings;
         $this->save();
     }
 
     private function checkSettingsExist(): void
     {
         throw_unless(
-            Schema::hasColumn($this->getTable(), 'settings'),
-            new \Exception("Table '{$this->getTable()}' does not have a 'settings' column")
+            Schema::hasColumn($this->getTable(), $this->settings_column),
+            new \Exception("Table '{$this->getTable()}' does not have a '{$this->settings_column}' column")
         );
 
         throw_unless(
-            in_array(Schema::getColumnType($this->getTable(), 'settings'), ['json', 'text']),
-            new \Exception('Settings column is not json type')
+            in_array(Schema::getColumnType($this->getTable(), $this->settings_column), ['json', 'text']),
+            new \Exception("'{$this->settings_column}' column is not json type")
         );
     }
 }
--- tests/HasSettingsTest.php
+++ tests/HasSettingsTest.php
@@ -6,11 +6,13 @@ use Mrdth\LaravelModelSettings\Tests\Models\ModelWithoutSettings;
 use Mrdth\LaravelModelSettings\Tests\Models\ModelWithSettings;
 
 beforeEach(function () {
+    $settings_column = config('model-settings.column');
+
     Schema::create(
         'model-with-settings',
-        function (Blueprint $table) {
+        function (Blueprint $table) use ($settings_column) {
             $table->increments('id');
-            $table->json('settings')->nullable();
+            $table->json($settings_column)->nullable();
             $table->timestamps();
         }
     );
@@ -18,21 +20,24 @@ beforeEach(function () {
 
 afterEach(function () {
     Schema::dropIfExists('model-with-settings');
+    Schema::dropIfExists('model-without-settings');
 });
 
-test('it initializes settings correctly', function () {
+it('initializes settings correctly', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
 
     // Check if settings is added to $fillable.
-    expect($model->getFillable())->toContain('settings');
+    expect($model->getFillable())->toContain($settings_column);
 
     // Check if settings is cast to array.
     $casts = $model->getCasts();
-    expect($casts)->toHaveKey('settings')
-        ->and($casts['settings'])->toEqual('array');
+    expect($casts)->toHaveKey($settings_column)
+        ->and($casts[$settings_column])->toEqual('array');
 });
 
-test('it checks model database table has settings column', function () {
+it('checks model database table has settings column', function () {
+    $settings_column = config('model-settings.column');
     Schema::create(
         'model-without-settings',
         function (Blueprint $table) {
@@ -42,35 +47,37 @@ test('it checks model database table has settings column', function () {
     );
 
     $this->expectException(\Exception::class);
-    $this->expectExceptionMessage("Table 'model-without-settings' does not have a 'settings' column");
+    $this->expectExceptionMessage("Table 'model-without-settings' does not have a '$settings_column' column");
 
     $model = new ModelWithoutSettings;
 });
 
-test('it checks model database table settings column is json type', function () {
+it('checks model database table settings column is json type', function () {
+    $settings_column = config('model-settings.column');
     Schema::create(
         'model-without-settings',
-        function (Blueprint $table) {
+        function (Blueprint $table) use ($settings_column) {
             $table->increments('id');
-            $table->string('settings')->nullable();
+            $table->string($settings_column)->nullable();
             $table->timestamps();
         }
     );
 
     $this->expectException(\Exception::class);
-    $this->expectExceptionMessage('Settings column is not json type');
+    $this->expectExceptionMessage("'$settings_column' column is not json type");
 
     $model = new ModelWithoutSettings;
 });
 
-test('it can get setting', function () {
+it('can get setting', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
-    $model->settings = ['foo' => 'bar'];
+    $model->$settings_column = ['foo' => 'bar'];
 
     expect($model->getSetting('foo'))->toEqual('bar');
 });
 
-test('it can add settings', function () {
+it('can add settings', function () {
     $model = new ModelWithSettings;
 
     $model->addSetting('foo', 'bar');
@@ -78,27 +85,30 @@ test('it can add settings', function () {
     expect($model->getSetting('foo'))->toEqual('bar');
 });
 
-test('it can update settings', function () {
+it('can update settings', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
-    $model->settings = ['foo' => 'bar'];
+    $model->$settings_column = ['foo' => 'bar'];
 
     $model->updateSetting('foo', 'baz');
 
     expect($model->getSetting('foo'))->toEqual('baz');
 });
 
-test('it can delete settings', function () {
+it('can delete settings', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
-    $model->settings = ['foo' => 'bar'];
+    $model->$settings_column = ['foo' => 'bar'];
 
     $model->deleteSetting('foo');
 
     expect($model->getSetting('foo'))->toBeNull();
 });
 
-test('it can delete all settings', function () {
+it('can delete all settings', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
-    $model->settings = ['foo' => 'bar', 'baz' => 'qux'];
+    $model->$settings_column = ['foo' => 'bar', 'baz' => 'qux'];
 
     $model->deleteSettings();
 
@@ -106,25 +116,28 @@ test('it can delete all settings', function () {
         ->and($model->getSetting('baz'))->toBeNull();
 });
 
-test('it throws exception when adding setting which already exists', function () {
+it('throws exception when adding setting which already exists', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
-    $model->settings = ['foo' => 'bar'];
+    $model->$settings_column = ['foo' => 'bar'];
 
     $this->expectException(\Exception::class);
     $this->expectExceptionMessage("Setting 'foo' already exists");
     $model->addSetting('foo', 'baz');
 });
 
-test('it can get all settings', function () {
+it('can get all settings', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
-    $model->settings = ['foo' => 'bar', 'baz' => 'qux'];
+    $model->$settings_column = ['foo' => 'bar', 'baz' => 'qux'];
 
     expect($model->getSettings())->toEqual(['foo' => 'bar', 'baz' => 'qux']);
 });
 
-test('it can check if setting exists', function () {
+it('can check if setting exists', function () {
+    $settings_column = config('model-settings.column');
     $model = new ModelWithSettings;
-    $model->settings = ['foo' => 'bar'];
+    $model->$settings_column = ['foo' => 'bar'];
 
     expect($model->hasSetting('foo'))->toBeTrue()
         ->and($model->hasSetting('baz'))->toBeFalse();
